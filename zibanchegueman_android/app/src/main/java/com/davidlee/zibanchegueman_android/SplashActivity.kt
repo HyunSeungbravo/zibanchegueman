c7f3d1431c06c871be976f75928634e386217a4a
tjdfhr7497 <57169499+tjdfhr7497@users.noreply.github.com>
2019-11-29 16:05:10 +0900

    splash 삽입

---
+++ zibanchegueman_android/app/src/main/java/com/davidlee/zibanchegueman_android/SplashActivity.kt
@@ -0,0 +1,40 @@
+package com.davidlee.zibanchegueman_android
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    private var mDelayeHandler : Handler? = null
+    private  val SPLASH_DELAY : Long = 3000
+
+    internal val  mRunnable = Runnable {
+        if (!isFinishing){
+
+            val intent=Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        mDelayeHandler = Handler()
+
+        mDelayeHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+
+    }
+
+    public override fun onDestroy() {
+
+        if(mDelayeHandler !=null){
+            mDelayeHandler!!.removeCallbacks(mRunnable)
+        }
+
+        super.onDestroy()
+    }
+}
